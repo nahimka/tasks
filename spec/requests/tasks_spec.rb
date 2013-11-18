ab31c8113ebc9e5b8acfc8c982a27516e68dbba4
fima.baranov <nahimkaorama@gmail.com>
2013-11-19 00:39:15 +0700

    showing data from the table Task

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe "Tasks" do
   describe "GET /tasks" do
    it "display some tasks" do
+   	@task = Task.create :task => 'Go to bed'
+
    	visit tasks_path
    	page.should have_content 'go to bed'
    end
